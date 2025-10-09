a33e0f8ed47c480736832d1bd9d75c9b2617566c
thanhhaunv <thanhhaunv@gmail.com>
2025-10-09 17:39:33 +0700

    Update app.module.ts

--- grok/codes/services/api-gateway/src/app.module.ts
+++ grok/codes/services/api-gateway/src/app.module.ts
@@ -1,10 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
+import { AuthController } from './controllers/auth.controller';
+import { ProjectsController } from './controllers/projects.controller';
+import { ChatController } from './controllers/chat.controller';
+import { AuthMiddleware } from './middleware/auth.middleware';
+import { RateLimitMiddleware } from './middleware/rate-limit.middleware';
+import { ErrorMiddleware } from './middleware/error.middleware';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [],
+  imports: [ConfigModule.forRoot({ load: [configuration] })],
+  controllers: [AppController, AuthController, ProjectsController, ChatController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware, RateLimitMiddleware, ErrorMiddleware)
+      .forRoutes('*');
+  }
+}
