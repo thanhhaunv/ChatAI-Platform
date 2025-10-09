e9027789f68ba57415c53508f9e0b91fc65b6e85
thanhhaunv <thanhhaunv@gmail.com>
2025-10-09 17:37:49 +0700

    Create error.middleware.ts

---
+++ grok/codes/services/api-gateway/src/middleware/error.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class ErrorMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    try {
+      next();
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
