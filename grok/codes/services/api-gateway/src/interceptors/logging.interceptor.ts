dfc0a55abdd89a20341cd6c2b71a66f7db668d06
thanhhaunv <thanhhaunv@gmail.com>
2025-10-09 17:38:29 +0700

    Create logging.interceptor.ts

---
+++ grok/codes/services/api-gateway/src/interceptors/logging.interceptor.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(
+        tap(() => console.log(`Request to ${req.url} took ${Date.now() - now}ms`)),
+      );
+  }
+}
