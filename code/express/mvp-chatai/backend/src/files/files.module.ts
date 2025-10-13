ab975c91c639c4111041c1ff5e80ca7486539df6
thanhhaunv <thanhhaunv@gmail.com>
2025-10-13 17:30:31 +0700

    Create files.module.ts

---
+++ code/express/mvp-chatai/backend/src/files/files.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [FilesController],
+  providers: [FilesService],
+  exports: [FilesService],
+})
+export class FilesModule {}
